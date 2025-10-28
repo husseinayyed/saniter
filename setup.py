3082ad3fb8647f5a927ab0ca46d4685b1cbf25d9
husseinayyed <honor9x.hasson@gmail.com>
2025-10-29 01:38:32 +0300

    Patch 0.1.1: Fix some typos

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+
 setup(
     name="saniter",
-    version="0.0.9",
+    version="0.1.1",
     packages=find_packages(),
+    package_data={
+        'saniter': [
+            'models/*.joblib',
+            'models/*.pkl',
+        ],
+    },
+    include_package_data=True,  # This is crucial!
     install_requires=[
         "scikit-learn>=1.0.0",
         "pandas>=1.3.0",
